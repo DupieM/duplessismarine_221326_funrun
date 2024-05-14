a68df6e09b6e276006274e601885be24f4fdc7ef
DupieM <221326@virtualwindow.co.za>
2024-05-14 22:52:15 +0200

    Log In Screen

--- funrun/screens/LogInScreen.jsx
+++ funrun/screens/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Image, ScrollView, TextInput, } from 'react-native'
+import { StyleSheet, View, Text, Image, ScrollView, TextInput, TouchableOpacity, } from 'react-native'
 import React from 'react'
 
 function LogInScreen() {
@@ -13,13 +13,20 @@ function LogInScreen() {
       </View>
       <View>
         <TextInput style={styles.search} placeholder='Password'/>
-        <Text>Forgot Password?</Text>
+        <Text style={styles.password}>Forgot Password?</Text>
       </View>
       <View>
-
+        <TouchableOpacity style={styles.Btn} onPress={() => console.log("Button Pressed")}>
+          <Text style={styles.Btntext}>Log In</Text>
+        </TouchableOpacity>
       </View>
-      <View>
-        <Text>Don't have an account Sign Up</Text>
+      <View style={styles.signupcontainer}>
+        <Text style={styles.signup}>
+          Don't have an account
+        </Text>
+        <Text style={styles.link}>
+          Sign Up
+        </Text>
       </View>
     </ScrollView>
   )
@@ -50,10 +57,44 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingLeft: 20,
     paddingRight: 10,
-    marginLeft: 30,
+    marginLeft: 35,
     borderRadius: 30,
     width: '80%',
     color: '#FFA948',
   },
-
+  password: {
+    marginLeft: 50,
+    marginTop: 5,
+    marginBottom: 27,
+    fontSize: 15,
+    color: '#263A38'
+  },
+  Btn: {
+    backgroundColor: '#263A38',
+    width: 240,
+    marginLeft: 55,
+    padding: 6,
+    borderRadius: 50,
+    marginBottom: 25
+  },
+  Btntext: {
+    fontSize: 30,
+    fontWeight: '900',
+    textAlign: 'center',
+    color: '#FFA948'
+  },
+  signupcontainer: {
+    flexDirection: 'row',
+    marginLeft: 77
+  },
+  signup: {
+    textAlign: 'center',
+    color: '#194D10'
+  },
+  link: {
+    marginLeft: 6,
+    textAlign: 'center',
+    color: '#194D10',
+    textDecorationLine: 'underline',
+  }
 })
