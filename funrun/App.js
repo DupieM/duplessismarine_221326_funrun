34ef908d67d93186b19d94fa0e4ed4c3e0450df0
DupieM <221326@virtualwindow.co.za>
2024-05-14 20:50:12 +0200

    Icon and Splash Screen

--- funrun/App.js
+++ funrun/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LogInScreen from './screens/LogInScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LogInScreen/>
   );
 }
 
--- funrun/screens/LogInScreen.jsx
+++ funrun/screens/LogInScreen.jsx
@@ -1,11 +1,27 @@
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image, ScrollView, TextInput, } from 'react-native'
 import React from 'react'
 
 function LogInScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Image source={require('../assets/background.png')}/>
+      <View>
+        <Text style={styles.mainhead}>Welcome</Text>
+      </View>
+      <View style={styles.searchfield}>
+        <TextInput style={styles.search} placeholder='Email'/>
+      </View>
+      <View>
+        <TextInput style={styles.search} placeholder='Password'/>
+        <Text>Forgot Password?</Text>
+      </View>
+      <View>
+
+      </View>
+      <View>
+        <Text>Don't have an account Sign Up</Text>
+      </View>
+    </ScrollView>
   )
 }
 
@@ -14,14 +30,30 @@ export default LogInScreen
 
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: "#151718"
+    flex: 1,
+    backgroundColor: '#7A9966'
   },
-  title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      color: "#ECEDEE"
-  }
+  mainhead: {
+    color: '#FFA948',
+    fontSize: 60,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 7,
+    marginBottom: 7
+  },
+  searchfield: {
+    marginBottom: 15
+  },
+  search: {
+    backgroundColor: '#194D10',
+    height: 60,
+    fontSize: 18,
+    paddingLeft: 20,
+    paddingRight: 10,
+    marginLeft: 30,
+    borderRadius: 30,
+    width: '80%',
+    color: '#FFA948',
+  },
+
 })
