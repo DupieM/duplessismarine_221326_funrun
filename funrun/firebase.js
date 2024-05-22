ff9318fd2bf4308bb6c0437ea560ad34c301c9e5
DupieM <221326@virtualwindow.co.za>
2024-05-22 10:23:25 +0200

    Linking my Firebase project to my Funrun app

---
+++ funrun/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaBEowdcVOLY6hlSnJqRQoxb_BwP7-m_8",
+  authDomain: "funrun-724b9.firebaseapp.com",
+  projectId: "funrun-724b9",
+  storageBucket: "funrun-724b9.appspot.com",
+  messagingSenderId: "48756187378",
+  appId: "1:48756187378:web:a7878a4714b4f6896b706b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app)
