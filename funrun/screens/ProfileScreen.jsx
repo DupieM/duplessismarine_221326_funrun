9806828ade1dce439554b85a22036b8ea61db449
DupieM <221326@virtualwindow.co.za>
2024-05-28 23:53:41 +0200

    Implementing the frontend styling of Results and Profile Screen

--- funrun/screens/ProfileScreen.jsx
+++ funrun/screens/ProfileScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, Button, Image, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { handleSignOut } from '../services/authService'
 
@@ -10,13 +10,65 @@ const handleSignout = () => {
 }
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>My Page</Text>
-      <Button
-        title="Sign Out"
-        color="green"
-        onPress={handleSignout} />
-    </View>
+      <Image 
+        style={styles.profimg} 
+        source={require('../assets/Dummy_avatar.png')}
+      />
+      <Image 
+        style={styles.addimg} 
+        source={require('../assets/add.png')}
+      />
+      <View style={styles.enterfield}>
+        <Text style={styles.enter}>Name</Text>
+      </View>
+      <View style={styles.enterfield}>
+        <Text style={styles.enter}>Email</Text>
+      </View>
+      <View style={styles.enterfield2}>
+        <Text style={styles.enter}>Password</Text>
+        <Image 
+          style={styles.editimg} 
+          source={require('../assets/edit.png')}
+        />
+      </View>
+      <Text style={styles.subhead}>Badges</Text>
+      <View style={styles.badgebox}>
+        <Image 
+          style={styles.badgeimg} 
+          source={require('../assets/emblem.png')}
+        />
+        <Image 
+          style={styles.badgeimg} 
+          source={require('../assets/emblem.png')}
+        />
+      </View>
+      <Text style={styles.subhead}>Previous Results</Text>
+      <View style={styles.contestant}>
+          <Text style={styles.number}>OC-Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>1</Text>
+      </View>
+      <View style={styles.contestant}>
+          <Text style={styles.number}>OC-Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>2</Text>
+      </View>
+      <View style={styles.contestant}>
+          <Text style={styles.number}>OC-Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>4</Text>
+      </View>
+      <View style={styles.contestant}>
+          <Text style={styles.number}>OC-Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>9</Text>
+      </View>
+      <TouchableOpacity style={styles.btn} onPress={handleSignout}>
+          <Text style={styles.btntext}>Sign Out</Text>
+      </TouchableOpacity>
+    </ScrollView>
   )
 }
 
@@ -26,13 +78,96 @@ export default ProfileScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#B9E79C"
   },
   title: {
-      fontSize: 38,
-      fontWeight: 'bold',
-      color: "#194D10"
-  }
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: "#194D10",
+    textAlign: 'center',
+    marginTop: 30
+  },
+  profimg: {
+    height: 210,
+    width: 210,
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 75
+  },
+  addimg: {
+    height: 50,
+    width: 53,
+    marginTop: -66,
+    marginLeft: 220,
+    marginBottom: 20
+  },
+  enterfield: {
+    marginBottom: 18,
+  },
+  enterfield2: {
+    marginBottom: 18,
+    flexDirection: 'row',
+  },
+  enter: {
+    backgroundColor: 'rgba(255, 191, 96, 0.3)',
+    height: 50,
+    fontSize: 32,
+    padding: 2,
+    paddingLeft: 20,
+    marginLeft: 35,
+    borderRadius: 10,
+    width: '80%',
+    color: '#00272E',
+  },
+  subhead: {
+    fontSize: 37,
+    fontWeight: '500',
+    color: "#A64510",
+    textAlign: 'left',
+    marginLeft: 35,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  badgebox: {
+    flexDirection: 'row',
+    marginLeft: 35
+  },
+  badgeimg: {
+    marginRight: 30,
+  },
+  editimg: {
+    width: 32,
+    height: 31,
+    marginLeft: -40,
+    marginTop: 8
+  },
+  contestant: {
+    flexDirection: 'row',
+    paddingLeft: 10,
+    backgroundColor: 'rgba(27, 102, 107, 0.3)',
+    marginBottom: 18,
+    marginLeft: 35,
+    marginRight: 35
+  },
+  number: {
+    fontSize: 28,
+    fontWeight: '500',
+    marginRight: 33,
+    color: '#00272E'
+  },
+  btn: {
+    marginTop: 20,
+    backgroundColor: '#263A38',
+    width: 200,
+    marginLeft: 85,
+    padding: 6,
+    borderRadius: 50,
+    marginBottom: 30
+  },
+  btntext: {
+    fontSize: 30,
+    fontWeight: '900',
+    textAlign: 'center',
+    color: '#FFA948'
+  },
 })
--- funrun/screens/ResultScreen.jsx
+++ funrun/screens/ResultScreen.jsx
@@ -5,30 +5,74 @@ function ResultScreen() {
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>Results Board</Text>
-      <View>
+      <View style={styles.positionone}>
+        <Image
+          style={styles.crown} 
+          source={require('../assets/crown.png')}
+        />
         <Image
           style={styles.profimg} 
           source={require('../assets/Dummy_avatar.png')}
         />
         <Text style={styles.profname}>Name</Text>
       </View>
-      {/* <View>
+      <View style={styles.positiontwo}>
         <Image
           style={styles.profimg} 
           source={require('../assets/Dummy_avatar.png')}
         />
         <Text style={styles.profname}>Name</Text>
       </View>
-      <View>
+      <View style={styles.positionthree}>
         <Image
           style={styles.profimg} 
           source={require('../assets/Dummy_avatar.png')}
         />
         <Text style={styles.profname}>Name</Text>
-      </View> */}
-      <Image/>
-      <View>
-        
+      </View>
+      <Image 
+        style={styles.podium} 
+        source={require('../assets/podium.png')}
+      />
+      <View style={styles.rest}>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>4</Text>
+        </View>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>5</Text>
+        </View>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>6</Text>
+        </View>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>7</Text>
+        </View>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>8</Text>
+        </View>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>9</Text>
+        </View>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>Name</Text>
+          <Text style={styles.number}>Time</Text>
+          <Text style={styles.number}>10</Text>
+        </View>
+        <View style={styles.contestant}>
+          <Text style={styles.number}>etc.</Text>
+        </View>
       </View>
     </ScrollView>
   )
@@ -45,16 +89,55 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
     marginTop: 37,
-    fontSize: 38,
+    fontSize: 50,
     fontWeight: 'bold',
     color: "#194D10"
   },
+  positionone: {
+    marginTop: 30,
+    marginLeft: 130
+  },
+  positiontwo: {
+    marginLeft: 6,
+    marginTop: -40
+  },
+  positionthree: {
+    marginLeft: 255,
+    marginTop: -70
+  },
   profimg: {
+    marginLeft: 8,
     height: 80,
     width: 80,
-    marginTop: 20
+    marginTop: 10
+  },
+  crown: {
+    width: 95,
+    height: 86
   },
   profname: {
+    marginLeft: 12,
     fontSize: 28,
+  },
+  podium: {
+    marginTop: -135,
+    width: 360,
+    height: 320
+  },
+  rest: {
+    backgroundColor: '#263A38',
+    padding: 12
+  },
+  contestant: {
+    flexDirection: 'row',
+    paddingLeft: 20,
+    backgroundColor: 'rgba(166, 69, 16, 0.2)',
+    marginBottom: 12
+  },
+  number: {
+    fontSize: 28,
+    fontWeight: '500',
+    marginRight: 70,
+    color: '#FFA948'
   }
 })
