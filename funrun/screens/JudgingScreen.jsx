35ad854f30625d52a3a2df03b7ad9217faab8693
DupieM <221326@virtualwindow.co.za>
2024-06-05 12:25:45 +0200

    strarting with judging screen frontend

--- funrun/screens/JudgingScreen.jsx
+++ funrun/screens/JudgingScreen.jsx
@@ -5,6 +5,16 @@ function JudgingScreen() {
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>Judging</Text>
+      <View style={styles.enterfield}>
+        <Text style={styles.enter}>Course Name</Text>
+      </View>
+      <View style={styles.enterfield}>
+        <Text style={styles.enter}>Contestant Name</Text>
+      </View>
+      <View style={styles.box1}>
+        
+      </View>
+
     </ScrollView>
   )
 }
@@ -21,6 +31,21 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: "#194D10",
     textAlign: 'center',
-    marginTop: 30
+    marginTop: 30,
+    marginBottom: 20
+  },
+  enterfield: {
+    marginBottom: 18,
+  },
+  enter: {
+    backgroundColor: 'rgba(255, 191, 96, 0.3)',
+    height: 50,
+    fontSize: 32,
+    padding: 2,
+    paddingLeft: 20,
+    marginLeft: 35,
+    borderRadius: 10,
+    width: '80%',
+    color: '#00272E',
   },
 })
